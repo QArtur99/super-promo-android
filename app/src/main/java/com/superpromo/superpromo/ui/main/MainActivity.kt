3151d2eaf9d5e3a77d687643290a01289337af82
QArtur99 <artur2113@o2.pl>
2021-01-21 18:06:20 +0100

    Update main view

--- app/src/main/java/com/superpromo/superpromo/ui/main/MainActivity.kt
+++ app/src/main/java/com/superpromo/superpromo/ui/main/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.superpromo.superpromo.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.superpromo.superpromo.R
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val sharedViewModel: SharedViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharedViewModel.run { }
     }
 }
